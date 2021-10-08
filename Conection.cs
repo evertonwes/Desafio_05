0a7bbe2a4967d3aadae86a1d62dcaa9bfa77773c
evertonwes <88938675+evertonwes@users.noreply.github.com>
2021-10-08 20:13:43 -0300

    Add files via upload

---
+++ Conection.cs
@@ -0,0 +1,29 @@
+﻿using MySql;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySqlConexaoCsharp
+
+
+{
+    static class Conection
+    {
+        static private string servidor = "localhost";
+        static private string bancoDados = "cadastro";
+        static private string usuario = "root";
+        static private string senha = "";
+
+
+
+        static public string srtConection = $"server={servidor}; database={bancoDados}; User={usuario}; password={senha}";
+        
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using MySql;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySqlConexaoCsharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Conectar())
+                MessageBox.Show("Conexão Bem Sucedida!");
+        }
+
+        private bool Conectar()
+        {
+            var result = false;
+
+            try
+            {
+                using (var cn = new MySqlConnection(Conection.srtConection))
+                {
+                    cn.Open();
+                    result = true;
+                }                                              
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            return result;
+        }
+    }
+}
